4dfae0fac3c3e0ad67772a1d25637540218e3f6d
rsk2002 <rahulskumar2002@gmail.com>
2022-05-21 16:42:21 +0530

    Created detumbling new file & before changing detumbling on-off to 2 axis

---
+++ Detumbling-Simulation/Satellite.m
@@ -0,0 +1,69 @@
+function derivative = Satellite(t,state)
+
+global BB invI I Bfieldmeasured pqrdotmeasured trgt
+global Bfieldnav pqrdotnav current voltage muB torq
+
+%%Earth and sat params
+Earth
+Inertia
+
+x = state(1);
+y = state(2);
+z = state(3);
+
+q0123 = state(7:10);
+pdot = state(11);
+qdot = state(12);
+rdot = state(13);
+pqrdot = state(11:13);
+
+%%translation
+vel = state(4:6);
+r = state(1:3);
+rho = norm(r);
+rhat = r/rho;
+Fgrav = -(G*M*m/rho^2)*rhat;
+acc = Fgrav/m;
+
+%%Mag stuff
+BI = [0;0;0];
+BI(1) = 0.001;
+BB = TIBquat(q0123)'*BI;
+
+if (pqrdot(3) < 0.001)
+    trgt = 1;
+end
+
+%%Bfield and ang velocty measured from snsr
+[Bfieldmeasured,pqrdotmeasured] = Sensor(BB,pqrdot);
+
+%%Navigation Block (correction)
+[Bfieldnav,pqrdotnav] = Navigation(Bfieldmeasured,pqrdotmeasured);
+
+
+%%CONTROL BLOCK
+muB_cont = Control(Bfieldnav,pqrdotnav);
+Magtorquer_params
+current_cont = muB_cont/(n*A*tempVar);
+voltage_cont = current_cont*Rnet;
+
+voltage = Analog(voltage_cont);
+current = voltage/Rnet;
+muB = current*n*A*tempVar;
+
+LMN_magtorquers = cross(muB,BB);
+
+%%%Rotational Dynamics
+PQRMAT = [0 -pdot -qdot -rdot;pdot 0 rdot -qdot;qdot -rdot 0 pdot;rdot qdot -pdot 0];
+q0123dot = 0.5*PQRMAT*q0123;
+if abs(pqrdot(3)) > 1*pi/180;
+    bearingFriction = I*[0;0;-5*pi/180];
+else 
+    bearingFriction = [0;0;0];
+end
+H = I*pqrdot;
+torq = LMN_magtorquers + bearingFriction - cross(pqrdot,H);
+pqrddot = invI*torq;
+
+derivative = [vel;acc;q0123dot;pqrddot];
+
